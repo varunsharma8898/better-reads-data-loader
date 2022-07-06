38fd2534c82eb15972eadcbb258c9b879f456361
Varun Sharma <varun.sharma@sincrodigital.com>
2022-07-06 11:40:13 +0530

    Adding vos

---
+++ src/main/java/com/varun/selfstudy/vo/Author.java
@@ -0,0 +1,32 @@
+package com.varun.selfstudy.vo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table("author_by_id")
+public class Author {
+
+    @Id
+    @PrimaryKeyColumn(name = "author_id", ordinal = 0, type = PrimaryKeyType.CLUSTERED)
+    private String id;
+
+    @Column("author_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String name;
+
+    @Column("personal_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String personalName;
+
+}
